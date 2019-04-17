c55226ecdef2afe2385d123072003a7fc4b80ef1
Lea Steffen <steffen@fzi.de>
2019-04-17 02:35:12 -0700

    added checkbox to hide an show advanced params

--- src/main/java/com/fzi/generateScript/impl/GenerateScriptProgramNodeContribution.java
+++ src/main/java/com/fzi/generateScript/impl/GenerateScriptProgramNodeContribution.java
@@ -1,21 +1,39 @@
 package com.fzi.generateScript.impl;
 
 
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import com.ur.urcap.api.contribution.ProgramNodeContribution;
 import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
 import com.ur.urcap.api.domain.ProgramAPI;
 import com.ur.urcap.api.domain.data.DataModel;
 import com.ur.urcap.api.domain.script.ScriptWriter;
+import com.ur.urcap.api.domain.userinteraction.keyboard.KeyboardInputCallback;
+import com.ur.urcap.api.domain.userinteraction.keyboard.KeyboardInputFactory;
+import com.ur.urcap.api.domain.userinteraction.keyboard.KeyboardTextInput;
 
 public class GenerateScriptProgramNodeContribution implements ProgramNodeContribution{
+	private static final String ADVANCED_PARAM_KEY = "showadvancedparam";
+	private static final boolean ADVANCED_PARAM_DEFAULT = false;
+	private static final String MAX_LOST_PACKAGES= "";
+	private static final String MAX_LOST_PACKAGES_DEFAULT_VALUE= "1000";
+	private static final String GAIN_SERVO_J= "";
+	private static final String GAIN_SERVO_J_DEFAULT_VALUE= "0";
 	private final ProgramAPI programAPI;
 	private final DataModel model;
+	private final GenerateScriptProgramNodeView view;
+	private final KeyboardInputFactory keyboardFactory;
+	
 
 	
 	public GenerateScriptProgramNodeContribution(ProgramAPIProvider apiProvider, GenerateScriptProgramNodeView view,
 			DataModel model) {
 		this.programAPI = apiProvider.getProgramAPI();
 		this.model = model;
+		this.view = view;
+		this.keyboardFactory = apiProvider.getUserInterfaceAPI().getUserInteraction().getKeyboardInputFactory();
+		updateAdvancedParam(getAdvancedParam());
 	}
 	
 
@@ -46,4 +64,72 @@ public class GenerateScriptProgramNodeContribution implements ProgramNodeContrib
 		return programAPI.getInstallationNode(GenerateScriptInstallationNodeContribution.class);
 	}
 	
+	
+	public ItemListener getListenerForAdvancedParam() {
+		return new ItemListener() {	
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					setAdvancedParam(true);
+				} else {
+					setAdvancedParam(false);
+				}
+			}
+		}; 
+	}
+	
+	
+	public void setParam(String key, String value) {
+		if ("".equals(value)) {
+			resetToDefaultValue(key);
+		} else {
+			model.set(key, value);  // TODO enum or so for key????
+		}
+	}
+	
+	public String getParam(String key) {
+		return model.get(MAX_LOST_PACKAGES, MAX_LOST_PACKAGES_DEFAULT_VALUE);
+	}
+	
+	private void resetToDefaultValue(String key) {
+		model.set(MAX_LOST_PACKAGES, MAX_LOST_PACKAGES_DEFAULT_VALUE);
+	}
+	
+	
+	
+	private boolean getAdvancedParam() {
+		return model.get(ADVANCED_PARAM_KEY, ADVANCED_PARAM_DEFAULT);
+	}
+	
+	public void setAdvancedParam(boolean show) {
+		updateAdvancedParam(show);
+		model.set(ADVANCED_PARAM_KEY, show);
+	}
+	
+	
+	private void updateAdvancedParam(boolean enable) {
+		System.out.println("Advnced Param is set to: " + enable);
+		if(enable) {
+			//TODO show advanced param
+		} else {
+			//TODO hide advanced param
+		}
+	}
+	
+	public KeyboardTextInput getInputForTextField() {
+		KeyboardTextInput keyboardInput = keyboardFactory.createStringKeyboardInput();
+		keyboardInput.setInitialValue(getParam(MAX_LOST_PACKAGES));
+		return keyboardInput;
+	}
+	
+	public KeyboardInputCallback<String> getCallbackForTextField() {
+		return new KeyboardInputCallback<String>() {
+			@Override
+			public void onOk(String value) {
+				System.out.println("VALUE: " + value);
+				setParam(MAX_LOST_PACKAGES, value);
+				view.UpdateMaxLostPackages_TF(value);;
+			}
+		};
+	}
 }
--- src/main/java/com/fzi/generateScript/impl/GenerateScriptProgramNodeView.java
+++ src/main/java/com/fzi/generateScript/impl/GenerateScriptProgramNodeView.java
@@ -1,35 +1,101 @@
 package com.fzi.generateScript.impl;
 
-
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Panel;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import com.ur.urcap.api.contribution.ContributionProvider;
 import com.ur.urcap.api.contribution.ViewAPIProvider;
 import com.ur.urcap.api.contribution.program.swing.SwingProgramNodeView;
+import com.ur.urcap.api.domain.userinteraction.keyboard.KeyboardTextInput;
 
 public class GenerateScriptProgramNodeView implements SwingProgramNodeView<GenerateScriptProgramNodeContribution>{
 
-	private final ViewAPIProvider apiProvider;
-	//private JLabel previewTitle;
+	//private final ViewAPIProvider apiProvider;
+	private JCheckBox advancedParam_CB; 
+	// general parameters
+	private JTextField maxLostPackages_TF = new JTextField(15);
+	
+	// advanced parameters
+	private JTextField gainServoj_TF= new JTextField(15);
+
 	
 	public GenerateScriptProgramNodeView(ViewAPIProvider apiProvider) {
-		this.apiProvider = apiProvider;
+		//this.apiProvider = apiProvider;
 	}
 
 	@Override
 	public void buildUI(JPanel panel, ContributionProvider<GenerateScriptProgramNodeContribution> provider) {
-		panel.add(createInfo());
 		
+		createParam(panel, provider, maxLostPackages_TF,"Max Nr. of lost pkg: ", "1000");
+		panel.add(createAdvancedParamBox(provider));
+		//panel.add(createHorizontalSpacing(10));
+		//panel.add(createVerticalSpacing(10));
+		createParam(panel, provider, gainServoj_TF, "Gain servoj: ", "0");	
 	}
 	
-	private Box createInfo() {
-		Box infoBox = Box.createHorizontalBox();
-		infoBox.setAlignmentX(Component.LEFT_ALIGNMENT);
-		infoBox.add(new JLabel("This program will log the remote host's IP."));
-		return infoBox;
+
+	public void setAdvancedParam_CB(boolean checked) {
+		advancedParam_CB.setSelected(checked);
+	}
+	
+
+	private void createParam(JPanel panel, final ContributionProvider<GenerateScriptProgramNodeContribution> provider, 
+			final JTextField textField, String labelText, String defaultInput) {
+		JLabel label = new JLabel(labelText);
+		panel.add(label);
+		
+		textField.setText(defaultInput);
+		textField.setFocusable(false);
+		textField.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				KeyboardTextInput keyboardInput = provider.get().getInputForTextField();
+				keyboardInput.show(textField, provider.get().getCallbackForTextField());
+			}
+		});
+		panel.add(textField);
+	}
+	
+	private Box createAdvancedParamBox(ContributionProvider<GenerateScriptProgramNodeContribution> provider) {
+		Box advancedParamBox = Box.createHorizontalBox();
+		advancedParam_CB = new JCheckBox("Show advanced parameters");
+		advancedParam_CB.setHorizontalTextPosition(SwingConstants.RIGHT);
+		advancedParam_CB.setBorder(BorderFactory.createEmptyBorder());
+		advancedParam_CB.addItemListener(provider.get().getListenerForAdvancedParam());
+		advancedParam_CB.setMaximumSize(advancedParam_CB .getPreferredSize());
+		advancedParamBox.add(advancedParam_CB );
+		advancedParamBox.add(Box.createHorizontalGlue());
+		return advancedParamBox;
 	}
 
+	
+
+	private Component createVerticalSpacing(int height) {
+		return Box.createRigidArea(new Dimension(0, height));
+	}
+	
+	private Component createHorizontalSpacing(int width) {
+		return Box.createRigidArea(new Dimension(width, 0));
+	}
+	
+	public void UpdateMaxLostPackages_TF(String value) {
+		maxLostPackages_TF.setText(value);
+	}
+	
+	public void UpdateGainServoj_TF(String value) {
+		gainServoj_TF.setText(value);
+	}
 }
