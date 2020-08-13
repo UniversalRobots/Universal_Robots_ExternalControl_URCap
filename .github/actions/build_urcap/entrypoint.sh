b00ab647ee1c30e47ab0c7b237544d6aba649b2e
Felix Exner <exner@fzi.de>
2020-08-13 18:42:52 +0200

    Download and install SDK from tempfolder
    This seems cleaner than assuming HOME would be fine for this.

--- .github/actions/build_urcap/entrypoint.sh
+++ .github/actions/build_urcap/entrypoint.sh
@@ -6,7 +6,8 @@ SDK_MAJOR=$(echo ${SDK_VERSION} | awk -F. '{print $1}')
 SDK_MINOR=$(echo ${SDK_VERSION} | awk -F. '{print $2}')
 SDK_BUILD=$(echo ${SDK_VERSION} | awk -F. '{print $3}')
 
-pushd ${HOME}
+download_folder=$(mktemp -d)
+pushd ${download_folder}
 curl -o sdk-${SDK_VERSION}.zip https://s3-eu-west-1.amazonaws.com/urplus-developer-site/sdk/sdk-${SDK_VERSION}.zip
 mkdir sdk
 unzip -q sdk-${SDK_VERSION}.zip -d sdk
