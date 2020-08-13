a403dd01cc4978077c674a0bfcd314ff420898db
Felix Exner <felix_mauch@web.de>
2020-08-13 16:56:53 +0200

    Update src/main/java/com/fzi/externalcontrol/impl/RequestProgram.java

--- src/main/java/com/fzi/externalcontrol/impl/RequestProgram.java
+++ src/main/java/com/fzi/externalcontrol/impl/RequestProgram.java
@@ -29,7 +29,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetSocketAddress
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class RequestProgram {
