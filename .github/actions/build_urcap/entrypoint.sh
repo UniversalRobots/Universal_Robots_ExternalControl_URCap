a0579a0c0777c53403e0b33b725e5d6bd4e5e46f
Felix Exner <exner@fzi.de>
2020-08-13 18:26:15 +0200

    Select subfolder depending on version

--- .github/actions/build_urcap/entrypoint.sh
+++ .github/actions/build_urcap/entrypoint.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+SDK_MAJOR=$(echo ${SDK_VERSION} | awk -F. '{print $1}')
+SDK_MINOR=$(echo ${SDK_VERSION} | awk -F. '{print $2}')
+SDK_BUILD=$(echo ${SDK_VERSION} | awk -F. '{print $3}')
+
 pushd ${HOME}
 curl -o sdk-${SDK_VERSION}.zip https://s3-eu-west-1.amazonaws.com/urplus-developer-site/sdk/sdk-${SDK_VERSION}.zip
 mkdir sdk
-unzip sdk-${SDK_VERSION}.zip -d sdk
-cd sdk
+unzip -q sdk-${SDK_VERSION}.zip -d sdk
+if [ "$SDK_MAJOR" -eq 1 ] && [ "$SDK_MINOR" -lt 10 ]; then
+  cd sdk
+else
+  cd sdk/URCap_SDK-${SDK_VERSION}
+fi
 ./install.sh
 
 popd
