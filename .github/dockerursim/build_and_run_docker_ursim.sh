1eeda4c386059b5dba03e2b00dcd52ecf058fcd4
mahp <mahp@universal-robots.com>
2022-02-04 10:57:42 +0100

    Make use of the ursim from dockerhub

--- .github/dockerursim/build_and_run_docker_ursim.sh
+++ .github/dockerursim/build_and_run_docker_ursim.sh
@@ -2,21 +2,18 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cp target/externalcontrol-*.jar ${DIR}/externalcontrol.jar
+# Make directory to store the URCap
+mkdir ${DIR}/urcaps 2>/dev/null
+cp target/externalcontrol-*.jar ${DIR}/urcaps/externalcontrol.jar
 
 docker network create --subnet=192.168.0.0/16 static_test_net
 
-docker build ${DIR} -t mydockerursim
-docker volume create dockerursim
 docker run --rm --name="mydockerursim" -d \
   -e ROBOT_MODEL=UR5 \
   --net static_test_net \
   --ip 192.168.56.101 \
   -v "${DIR}/.vol":/ursim/programs \
+  -v "${DIR}/urcaps":/urcaps \
   --privileged \
   --cpus=1 \
-  mydockerursim
-
-# This is needed in order to install the URCap properly
-sleep 30
-docker exec -t mydockerursim killall java
+  universalrobots/ursim_e-series
