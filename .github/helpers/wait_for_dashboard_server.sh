fb7706475d69594d51803e271d114abf5d79f045
Felix Exner <feex@universal-robots.com>
2025-12-01 16:49:31 +0100

    Refactor wait_for_dashboard_server

--- .github/helpers/wait_for_dashboard_server.sh
+++ .github/helpers/wait_for_dashboard_server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-netcat -z 127.0.0.1 29999
-while [ $? -eq 1 ]
+expected="Connected: Universal Robots Dashboard Server
+Disconnected"
+response=$(echo "quit" | netcat -q 1 127.0.0.1 29999)
+while [ "$response" != "$expected" ]
 do
   echo "Dashboard server not accepting connections..."
   sleep 3
-  netcat -z 127.0.0.1 29999
+  response=$(echo "quit" | netcat -q 1 127.0.0.1 29999)
 done
 echo "Dashboard server connections are possible."
-sleep 5
